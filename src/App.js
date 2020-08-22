8c36a9eb789e16dadc75489ba6de0866894b859c
Double D <adeth.slipz@gmail.com>
2020-08-23 01:41:35 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Button,FormControl, InputLabel, Input } from '@material-ui/core';
+import Message from './Message';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+
+  console.log(username);
+
+  useEffect(() => {
+    setUsername(prompt('Masukkan Username'))
+  }, [] )
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Messenger</h1>
+      <h2>Hello, {username}</h2>
+      <form>
+        <FormControl>
+          <InputLabel>Ketik Pesan...</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+          <Button disabled={!input} type='submit' variant="contained" color="primary" onClick={sendMessage}>Kirim Pesan</Button>
+        </FormControl>
+      </form>
+
+      {/* displaying messages */}
+      {
+        messages.map(message => (
+          <Message text={message}/>
+        ))
+      }
     </div>
   );
 }
